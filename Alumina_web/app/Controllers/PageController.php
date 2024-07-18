7fd5aab42a9bada56b751ff967c7fdf3ed8db029
lakmali-vishaka <119029100+lakmali-vishaka@users.noreply.github.com>
2024-07-18 10:48:21 +0530

    adding backend of regestered alumini displaying part

--- Alumina_web/app/Controllers/PageController.php
+++ Alumina_web/app/Controllers/PageController.php
@@ -13,5 +13,12 @@ class PageController extends Controller
     {
         return view('jAForm');
     }
+    
+    public function view_alumini()
+{
+    $users = User::all()->toArray(); // Fetch all users and convert to array
+    return view('alumini', compact('users')); // Pass the users data to the view
+
+}
 
 }
